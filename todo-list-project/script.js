7ea1b85c694dbb6b7a622e3fc0fe2e7a5f9ee730
jaovbcx <v.conde@academico.ifrr.edu.br>
2026-02-02 11:29:46 -0400

    second part using array and generate html to display the tasks on the web page

--- todo-list-project/script.js
+++ todo-list-project/script.js
@@ -1,10 +1,27 @@
 const todoArray = []; 
+let todoListHTML = '';
+let taskList = '';
+
+function displayTasks() {
+  for (i = 0; i < todoArray.length; i++) {
+    const taskName = todoArray[i];
+    const taskHTML = `<p>${taskName}</p>`;
+    taskList += taskHTML;
+  }
+  const taskContainer = document.querySelector('.tasks-container');
+  taskContainer.innerHTML = taskList; 
+  taskList = '';
+}
+
 
 function addTask() {
   inputElement = document.querySelector('.inputTaskPlace');
   taskName = inputElement.value;
   todoArray.push(taskName);
-  console.log(todoArray);
   inputElement.value = '';
+  displayTasks();
 }
+
+
+
 
