92822a9051cfe6b83fb899c4c1914879795db27f
jaovbcx <v.conde@academico.ifrr.edu.br>
2026-02-03 12:48:12 -0400

    dueDate is working now, object working inside todoArray

--- todo-list-project/script.js
+++ todo-list-project/script.js
@@ -1,21 +1,19 @@
-const todoArray = [{
-  name: '',
-  dueDate: ''
-  }
-]; 
+const todoArray = []; 
 let todoListHTML = '';
 let taskList = '';
 
 
 function displayTasks() {
   for (i = 0; i < todoArray.length; i++) {
-    const taskName = todoArray[i];
+    const task = todoArray[i];
+    const taskName = task.name;
+    const taskDueDate = task.dueDate;
     const taskHTML = 
-      `<p>${taskName}
-         <button onClick='todoArray.splice(${i}, 1);
+      `<p>${taskName} ${taskDueDate}
+        <button onClick='todoArray.splice(${i}, 1);
           displayTasks();'>
           delete
-         </button>
+        </button>
       </p>`;
     taskList += taskHTML;
   }
@@ -27,9 +25,17 @@ function displayTasks() {
 
 function addTask() {
   inputElement = document.querySelector('.inputTaskPlace');
-  taskName = inputElement.value;
-  todoObject.push(taskName);
+  dueDateInput = document.querySelector('.due-date');
+  const newTask = {
+    name: inputElement.value,
+    dueDate: dueDateInput.value
+  }
+
+  if (newTask.name === '' || newTask.dueDate === '') return;
+
+  todoArray.push(newTask);
   inputElement.value = '';
+  dueDateInput.value = '';
   displayTasks();
 }
 
